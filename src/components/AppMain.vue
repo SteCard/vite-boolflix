9015905d3785dd4b825f03c6e25bd3703c19742b
SteCard <stefanocardillo14@gmail.com>
2023-07-01 21:35:48 +0200

    poster/ratings  + MovieCard app

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,37 +1,22 @@
 <script>
 
 import { store } from '../store.js';
+import MovieCard from './MovieCard.vue'
 
     export default {
         name: 'AppMain',
+        
+        components: {
+            MovieCard,
+        },
+
         data() {
             return {
-                store
+                store,
             }
         },
         methods: {
 
-            getFlagEmoji(countryCode) {
-                if(countryCode === 'en'){
-                    countryCode = 'gb'
-                }else if(countryCode === 'ja'){
-                    countryCode = 'jp'
-                }else if(countryCode === 'zh'){
-                    countryCode = 'cn'
-                }else if(countryCode === 'ko'){
-                    countryCode = 'kr'
-                }else if(countryCode === 'hi'){
-                    countryCode = 'in'
-                }else if(countryCode === 'cs'){
-                    countryCode = 'cz'
-                }else if(countryCode === undefined){
-                    countryCode = ''
-                }
-                const codePoints = countryCode.toUpperCase().split('').map(char =>  127397 + char.charCodeAt());
-                return String.fromCodePoint(...codePoints);
-            }
-
-            
         },
     }
 </script>
@@ -40,17 +25,11 @@ import { store } from '../store.js';
     <main>
         <h1 v-if="store.movieRes.length > 0">Film trovati</h1>
         <div class="movieInfo" v-for="movie in store.movieRes">
-            <p>{{movie.original_title}}</p>
-            <p>{{movie.title}}</p>
-            <p>{{getFlagEmoji(movie.original_language)}}</p>
-            <p>{{movie.vote_average}}</p>
+            <MovieCard :title="movie" :titleOG="movie.original_title" :titleTitle="movie.title"/>
         </div>
         <h1 v-if="store.seriesRes.length > 0">Serie trovate</h1>
         <div class="seriesInfo" v-for="series in store.seriesRes">
-            <p>{{series.original_name}}</p>
-            <p>{{series.name}}</p>
-            <p>{{getFlagEmoji(series.original_language)}}</p>
-            <p>{{series.vote_average}}</p>
+            <MovieCard :title="series" :titleOG="series.original_name" :titleTitle="series.name"/>
         </div>
     </main>
 </template>
@@ -58,4 +37,12 @@ import { store } from '../store.js';
 <style lang="scss">
     @use '../styles/partials/mixing.scss' as *;
     @use '../styles/partials/variables.scss' as *;
+
+    .poster{
+        width: 200px;
+    }
+
+    .vote{
+        width: 120px;
+    }
 </style>
